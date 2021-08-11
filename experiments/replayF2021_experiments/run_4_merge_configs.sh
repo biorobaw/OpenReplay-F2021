33f6c0f2a1731d7cc2911c5621f144148c10b8bd
Chance Hamilton <titonka@Chances-MBP.home>
2021-08-11 14:53:37 -0400

    Partial Intial Commit add experiment folder

---
+++ experiments/replayF2021_experiments/run_4_merge_configs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#in git root folder execute the following commands:
+
+source "experiments/BICY2020_modified/run_set_variables.sh"
+
+
+module add apps/python/3.7.3
+
+for E in ${RUN[*]}; do
+	echo "python $SCRIPT_4_MERGE $(map $E LOG_FOLDER)/ "
+	python $SCRIPT_4_MERGE $(map $E LOG_FOLDER)/ 
+done
+
